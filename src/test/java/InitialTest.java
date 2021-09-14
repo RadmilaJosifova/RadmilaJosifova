a84e30d6501d615d2aecc2271c8585fb2fff2943
RadmilaJosifova <radmila.josifova@gmail.com>
2021-09-15 00:12:36 +0200

    vezba prv cas+home work

--- src/test/java/InitialTest.java
+++ src/test/java/InitialTest.java
@@ -1,8 +1,27 @@
+
+import client.PeopleApiClient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
 
 public class InitialTest {
+
+    PeopleApiClient peopleApiClient = new PeopleApiClient();
+    HttpResponse response;
+    HttpResponse getPeople;
+    HttpResponse getOnePerson;
     @Test
-    public void InitialTest(){
+    public void InitialTest() throws Exception{
+
+
+       // HttpResponse welcomeRequest = peopleApiClient.getWelcomeRequest();
+        response =peopleApiClient.getWelcomeRequest();
+        getPeople =peopleApiClient.getAllPeople();
+        getOnePerson = peopleApiClient.getOnePerson();
+      String body = EntityUtils.toString(response.getEntity());
+      String body1 = EntityUtils.toString(getPeople.getEntity());
+      String body2 = EntityUtils.toString(getOnePerson.getEntity());
 
     }
 }
